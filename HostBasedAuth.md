dc16a9e18c433dc24a2cd51709bf04b07002b6ef
Jephte Clain <Jephte.Clain@univ-reunion.fr>
2016-12-29 18:42:29 +0400

    finaliser l'implémentation et la documentation

---
+++ HostBasedAuth.md
@@ -0,0 +1,188 @@
+[Show english version](#en)
+
+# Trigger HostBasedAuth {#fr}
+
+Ce trigger permet d'authentifier sur la base de l'adresse ip ou du nom de l'hôte
+qui fait la requête. Il est conçu pour fonctionner avec le mode d'accès http
+
+Le principe est le suivant: pour les dépôts concernés:
+* il faut autoriser l'utilisateur `anyhost`
+* définir des options pour la liste des hôtes ou des adresses IPs qui sont
+  autorisés
+* si l'accès est effectué en mode anonyme avec l'utilisateur `anonhttp` depuis
+  les hôtes spécifiés, alors l'utilisateur est traduit en `anyhost`
+
+Dans l'exemple suivant:
+~~~
+repo repo1
+    R = anyhost
+    option allow-host 10.11.12.13
+repo repo2
+    RW = anyhost
+    option allow-host HOST.DOMAIN
+repo repo3
+    RW = anyhost
+    option allow-host HOSTNAME
+~~~
+le dépôt repo1 est accessible en lecture depuis l'adresse 10.11.12.13;
+le dépôt repo2 est accessible en écriture depuis toute adresse ip dont la résolution inverse est HOST.DOMAIN;
+le dépôt repo3 est accessible en écriture depuis toute adresse ip dont la résolution inverse est un domaine dont le nom de base est HOSTNAME
+
+## Installation
+
+* Ce trigger utilise NetAddr::IP. Sur debian jessie, il est possible de
+  l'installer avec la commande suivante:
+    ~~~
+    sudo apt-get install libnetaddr-ip-perl
+    ~~~
+  Si l'erreur suivante apparait lors de l'utilisation du module:
+    ~~~
+    Can't locate auto/NetAddr/IP/InetBase/AF_INET6.al in @INC (...)
+    at /usr/lib/x86_64-linux-gnu/perl5/5.20/NetAddr/IP/InetBase.pm line 81.
+    ~~~
+  Il s'agit d'un bug connu avec l'autoloader. Il suffit de corriger le fichier
+  InetBase.pm et rajouter
+    ~~~
+    use Socket;
+    ~~~
+  juste après
+    ~~~
+    package NetAddr::IP::InetBase;
+    ~~~
+  cf ci-dessous pour une procédure automatique de correction sur debian jessie,
+  valable au 29/12/2016
+
+* Si ce n'est déjà fait, configurer `LOCAL_CODE` dans gitolite.rc
+    ~~~
+    LOCAL_CODE => "$ENV{HOME}/local",
+    ~~~
+
+* Dans le répertoire `LOCAL_CODE`, copier HostBasedAuth.pm dans le
+  sous-répertoire lib/Gitolite/Triggers
+    ~~~
+    srcdir=PATH/TO/gitolite-contrib
+
+    localdir="$(gitolite query-rc LOCAL_CODE)"
+    [ -d "$localdir" ] &&
+        rsync -r "$srcdir/lib" "$localdir" ||
+        echo "LOCAL_DIR: not found nor defined"
+    ~~~
+
+* Activer le trigger HostBasedAuth dans gitolite.rc
+    ~~~
+    INPUT => [
+        'HostBasedAuth::input',
+    ],
+
+    # Uncomment if customization is needed
+    #HOST_BASED_AUTH => {
+    #    ANON_USER => 'anonhttp',
+    #    HOST_USER => 'anyhost',
+    #},
+    ~~~
+
+## Configuration
+
+* IMPORTANT: comme il s'agit d'un accès par http, il faut autoriser l'accès au
+  user `daemon` pour tous les dépôt concerncés, e.g
+    ~~~
+    repo gitolite-admin
+        - = daemon
+        option deny-rules = 1
+    repo @all
+        R = daemon
+    ~~~
+  ou spécifiquement pour un dépôt:
+    ~~~
+    repo myrepo
+        R = daemon
+        RW+ = anyhost
+        option allow-host = myhost.domain
+    ~~~
+
+* Une fois que la condition précédente est remplie, tous les dépôts qui doivent
+  être autorisés sur la base de l'hôte doivent remplir au moins deux conditions:
+    * Autoriser le user `anyhost` en fonction des accès à fournir
+    * Définir l'option `allow-host` pour définir les hôtes pour lesquels le user
+      `anonhttp` est traduit en `anyhost`
+
+### anonhttp
+
+Ce user est celui qui identifie les connexions http anonymes. C'est uniquement
+si la connexion est anonyme qu'une traduction du nom d'utilisateur est faite.
+
+En effet, on considère que si la connexion n'est pas anonyme, alors il n'est pas
+nécessaire d'authentifier plus encore.
+
+Par défaut, on prend la valeur de `%RC{HOST_BASED_AUTH}{ANON_USER}`, sinon la
+valeur de `%RC{HTTP_ANON_USER}`, sinon la valeur `anonhttp`
+
+### anyhost
+
+Ce user est celui qui doit recevoir les autorisation sur les dépôts.
+
+Par défaut, on prend la valeur de `%RC{HOST_BASED_AUTH}{HOST_USER}`, sinon la
+valeur `anyhost`.
+
+### option allow-host
+
+Cette option permet de définir une liste d'adresses IP, de classes d'adresses
+IP, de noms d'hôtes pleinement qualifiés, de domaines ou de noms d'hôtes pour
+lequels la traduction `anonhttp --> anyhost` est effectuée, c'est à dire qui
+sont autorisés à accéder au dépôt.
+
+Exemples:
+~~~
+option allow-host 10.11.12.13 192.168.1.50
+option allow-host-1 10.50.60.0/24
+option allow-host-2 hostname.domain .domain hostname
+~~~
+Cet exemple montre qu'il est possible de spécifier plusieurs lignes allow-host
+en les suffixant par '-qqchose'. Il est aussi possible de spécifier plusieurs
+valeurs dans une même option en les séparant par des espaces.
+
+Les règles de correspondance sont:
+* correspondance exacte pour les adresses IP
+* correspondance exacte de réseau pour la notation CIDR
+* correspondance exacte insensible à la casse pour les domaines
+
+### option match-repo
+
+Cette option permet de faire des correspondance d'hôte sur la base du nom du
+dépôt. Tout d'abord, le nom du dépôt est mis en correspondance avec l'expression
+régulière de l'option `match-repo`. Si une correspondance est trouvée, alors le
+nom d'hôte est construit à partir des groupes de correspondances numériques
+trouvés sur le nom du dépôt.
+
+Un exemple sera sans doute plus parlant:
+~~~
+repo hosts/..*
+    RW+ = anyhost
+    option match-repo = hosts/([^/]+)/config
+    option allow-host = $1.domain
+~~~
+Dans cet exemple, les dépôts de la forme 'hosts/HOST/config' sont accessibles et
+modifiables depuis les hôtes 'HOST.domain'
+
+## Patch de NetAddr::IP pour Debian Jessie
+
+Au 29/12/2016, le bug avec NetAddr::IP existe toujours sur debian jessie et
+peut-être corrigé de cette manière (ne pas oublier de corriger les chemins et/ou
+les versions des packages):
+~~~
+sudo patch <<EOF
+--- /usr/lib/x86_64-linux-gnu/perl5/5.20/NetAddr/IP/InetBase.pm.orig	2016-12-29 14:54:19.396359452 +0400
++++ /usr/lib/x86_64-linux-gnu/perl5/5.20/NetAddr/IP/InetBase.pm	2016-12-29 14:33:37.888900910 +0400
+@@ -1,5 +1,6 @@
+ #!/usr/bin/perl
+ package NetAddr::IP::InetBase;
++use Socket;
+ 
+ use strict;
+ #use diagnostics;
+EOF
+~~~
+
+# HostBasedAuth trigger {#en}
+
+-*- coding: utf-8 mode: markdown -*- vim:sw=4:sts=4:et:ai:si:sta:fenc=utf-8:noeol:binary
--- lib/Gitolite/Triggers/HostBasedAuth.pm
+++ lib/Gitolite/Triggers/HostBasedAuth.pm
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 mode: perl -*- vim:sw=4:sts=4:et:ai:si:sta:fenc=utf-8
 package Gitolite::Triggers::HostBasedAuth;
 
+use Socket;
+use NetAddr::IP::Lite;
 use Gitolite::Rc;
 use Gitolite::Common;
 use Gitolite::Conf::Load;
@@ -8,34 +10,126 @@ use Gitolite::Conf::Load;
 use strict;
 use warnings;
 
+sub __get_host {
+    my $ip = shift;
+    my $addr = inet_aton($ip);
+    my $host = gethostbyaddr($addr, AF_INET);
+    return $host
+}
+
+my $re_ip = qr/^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/;
+my $re_host = qr/^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/;
+sub __is_host {
+    my $host = shift;
+    # not an IP address first
+    return 0 if $host =~ $re_ip;
+    return $host =~ $re_host;
+}
+
+sub __replace_user {
+    my $user = shift;
+    $ENV{ORIG_REMOTE_USER} = $ENV{REMOTE_USER};
+    $ENV{REMOTE_USER} = $user;
+    $ARGV[0] = $user;
+}
+
 sub input {
     my $anon_user = $rc{HOST_BASED_AUTH}{ANON_USER} || $rc{HTTP_ANON_USER} || 'anonhttp';
-    my $host_user = $rc{HOST_BASED_AUTH}{HOST_USER} || 'anonhost';
+    my $host_user = $rc{HOST_BASED_AUTH}{HOST_USER} || 'anyhost';
+
+    gl_log("input", "host-based-auth");
 
     # only map anon_user
     my $user = $ARGV[0];
-    gl_log("input", "hba", "checking //$user// == $anon_user");
+    trace(1, "checking if user=$user is $anon_user");
     return if $user ne $anon_user;
 
+    # check ip
+    (my $clientip = $ENV{SSH_CONNECTION} || '') =~ s/ .*//;
+    my $clientipdesc = $clientip || "(None)";
+    trace(1, "got clientip=$clientipdesc");
+    return unless $clientip;
+    my $clienthost = __get_host($clientip);
+    (my $clientname = $clienthost) =~ s/\..*$//;
+    (my $clientdomain = $clienthost) =~ s/^[^.]+//;
+    trace(1, "clientip=$clientip resolves to clienthost=$clienthost");
+
+    # check verb
     my $git_commands = "git-upload-pack|git-receive-pack|git-upload-archive";
-    gl_log("input", "hba", "checking //$ENV{SSH_ORIGINAL_COMMAND}// for verb in ($git_commands)");
+    trace(1, "checking soc=\"$ENV{SSH_ORIGINAL_COMMAND}\" for verb in ($git_commands)");
     if ( $ENV{SSH_ORIGINAL_COMMAND} =~ /(?:$git_commands) '\/?(\S+)'$/ ) {
-        my $repo = $1;
-        $repo =~ s/\.git$//;
-            
-        my $ip = $ENV{SSH_CONNECTION} || '(no-IP)';
-        $ip =~ s/ .*//;
+        (my $repo = $1) =~ s/\.git$//;
+        trace(1, "got repo=$repo");
 
-        my $allows = git_config($repo, "^gitolite-options\\.allow-from([.-].*)?");
-        my @allows = map {split} values %$allows;
+        my $match_repo = option($repo, "match-repo");
+        my @matches;
+        if ($match_repo) {
+            trace(1, "got match-repo=$match_repo");
+            @matches = $repo =~ $match_repo;
+            trace(1, "got \@matches=(@matches)");
+            return unless @matches;
+        }
 
-        gl_log("input", "hba", "checking repo $repo for ip $ip in (".join(", ", @allows).")");
-        for my $allow (@allows) {
-            if ($allow eq $ip) {
-                gl_log("input", "hba", "found $ip match with $allow", "mapping to $host_user");
-                $ARGV[0] = $host_user;
-                $ENV{REMOTE_USER} = $host_user;
-                last;
+        my $allow_hosts = git_config($repo, "^gitolite-options\\.allow-host([.-].*)?");
+        my @allow_hosts = map {split} values %$allow_hosts;
+        trace(1, "got \@allow-hosts=(@allow_hosts)");
+
+        for my $allow (@allow_hosts) {
+            trace(1, "checking allow-host=$allow");
+            if ($match_repo) {
+                my $tallow = $allow;
+                for my $i (0 .. $#matches) {
+                    my $from = '\$'.($i + 1);
+                    my $to = $matches[$i];
+                    $tallow =~ s/$from/$to/e;
+                }
+                trace(1, "allow-host=$allow translates to allow-host=$tallow");
+                $allow = $tallow;
+            }
+            if (__is_host($allow)) {
+                if ($allow =~ /^\./) {
+                    # .domain match
+                    trace(1, "trying .domain match");
+                    if ($allow eq $clientdomain) {
+                        gl_log("input", "host-based-auth", "clientdomain=$clientdomain matches with allow-host=$allow", "mapping $anon_user to $host_user");
+                        __replace_user($host_user);
+                        last;
+                    } else {
+                        trace(1, "...not matched");
+                    }
+                } elsif ($allow =~ /\./) {
+                    # host.domain match
+                    trace(1, "trying host.domain match");
+                    if ($allow eq $clienthost) {
+                        gl_log("input", "host-based-auth", "clienthost=$clienthost matches with allow-host=$allow", "mapping $anon_user to $host_user");
+                        __replace_user($host_user);
+                        last;
+                    } else {
+                        trace(1, "...not matched");
+                    }
+                } else {
+                    # host match
+                    trace(1, "trying host match");
+                    if ($allow eq $clientname) {
+                        gl_log("input", "host-based-auth", "clientname=$clientname matches with allow-host=$allow", "mapping $anon_user to $host_user");
+                        __replace_user($host_user);
+                        last;
+                    } else {
+                        trace(1, "...not matched");
+                    }
+                }
+            } elsif (my $allowip = new NetAddr::IP::Lite($allow)) {
+                # ip match
+                trace(1, "$allow translates to $allowip");
+                my $maskip = new NetAddr::IP::Lite($clientip, $allowip->mask());
+                trace(1, "trying ip match");
+                if ($allowip->network() eq $maskip->network()) {
+                    gl_log("input", "host-based-auth", "matched clientip=$clientip with allow-host=$allow", "mapping $anon_user to $host_user");
+                    __replace_user($host_user);
+                    last;
+                } else {
+                    trace(1, "...not matched");
+                }
             }
         }
     }
